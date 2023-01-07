703118edccc9724d1fdc44eb292a39b3900d2bc6
Cindewi <112582932+dewismecindy@users.noreply.github.com>
2023-01-07 16:30:51 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
+import "./App.css";
+import { useState } from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faCalculator,
+  faMinus,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Header from "./components/Header";
-import Counter from "./components/Counter";
 import Footer from "./components/Footer";
+library.add(faCalculator, faMinus, faPlus);
+
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
       <Header />
-      <Counter />
+      <main>
+        <div className="counter">
+          <div>
+            <div
+              className="minus"
+              onClick={() => {
+                setCounter(counter - 1);
+              }}
+            >
+              {counter > 0 && (
+                <FontAwesomeIcon icon="fa-solid fa-minus" size="4x" />
+              )}
+            </div>
+            <p>{counter}</p>
+
+            <div
+              className="plus"
+              onClick={() => {
+                setCounter(counter + 1);
+              }}
+            >
+              {counter < 10 && (
+                <FontAwesomeIcon icon="fa-solid fa-plus" size="4x" />
+              )}
+            </div>
+          </div>
+          <button
+            className="reset"
+            onClick={() => {
+              setCounter(0);
+            }}
+          >
+            Reset
+          </button>
+        </div>
+      </main>
       <Footer />
     </div>
   );
 }
+
 export default App;
