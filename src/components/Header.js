ec1819727e06fbef1e18d662cd51a742343e0060
Cindewi <112582932+dewismecindy@users.noreply.github.com>
2023-01-11 00:26:30 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
-import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
   return (
-    <header className="header">
-      <img src="calculatrice.png" alt="" />
+    <header>
+      <FontAwesomeIcon
+        icon="fa-solid fa-calculator"
+        size="3x"
+        color="#5C48D3"
+      />
       <h1>React Counter</h1>
     </header>
   );
